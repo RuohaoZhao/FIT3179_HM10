b3a61845cce15ca7f2431606dcddea8093934506
RuohaoZhao <91322072+RuohaoZhao@users.noreply.github.com>
2021-10-09 22:14:23 +0800

    Update vega_lite_bubblechart.js

--- vega_lite_bubblechart.js
+++ vega_lite_bubblechart.js
@@ -1,4 +1,4 @@
 var vg_1 = "bubblechart_HM10.vg.json"; 
-vegaEmbed("#bubble", vg_1).then(function(result) { 
+vegaEmbed("#vis", vg_1).then(function(result) { 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
