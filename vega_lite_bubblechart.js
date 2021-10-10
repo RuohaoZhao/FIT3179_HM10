d1942d52fdfb054b23b393a351e623890764595e
RuohaoZhao <91322072+RuohaoZhao@users.noreply.github.com>
2021-10-10 10:35:22 +0800

    Delete vega_lite_bubblechart.js

--- vega_lite_bubblechart.js
+++
@@ -1,4 +0,0 @@
-var vg_1 = "bubblechart_HM10.vg.json"; 
-vegaEmbed("#bubble_chart", vg_1).then(function(result) { 
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
-}).catch(console.error);
